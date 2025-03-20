fd889bb56395b650eb9c133a50010658504abf76
jaqkstefan <36705379+jaqkstefan@users.noreply.github.com>
2025-03-20 09:33:33 +0100

    Update streamlit_app.py
    upd remove when_matched

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col, when_matched
+# from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 helpful_links = [
     "https://docs.streamlit.io",
@@ -23,7 +23,7 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be: ', name_on_order)
 
 #session = get_active_session()
-cnx = st.connection('snowflake')
+cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
